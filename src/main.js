093384f864fd3be230660eae540c262af4f68ccc
Jakub Norkiewicz <kubanorkiewicz@gmail.com>
2021-04-24 17:44:22 +0100

    fix vuex

--- src/main.js
+++ src/main.js
@@ -11,8 +11,14 @@ import NotFound from './components/nav/NotFound.vue';
 const store = createStore({
     state() {
         return {
-            dataset: ''
+            dataset: 'lol'
         };
+    },
+
+    mutations: {
+        setDataset(state, payload) {
+            state.dataset = payload.dataset;
+        }
     }
 });
 
@@ -20,15 +26,15 @@ const router = createRouter({
     history: createWebHistory() ,
     routes: [
         { path: '/', redirect: '/input' },
-        { path: '/input', component: Input },
-        { path: '/calculate', component: Calculate },
-        { path: '/result', component: Result },
+        { path: '/input', name: 'input', component: Input },
+        { path: '/calculate', name: 'calculate', component: Calculate },
+        { path: '/result', name: 'result', component: Result },
         { path: '/:notFound(.*)', component: NotFound },
     ]
 });
 
 const app = createApp(App)
 
-app.use(router, store);
+app.use(router).use(store);
 
 app.mount('#app');
