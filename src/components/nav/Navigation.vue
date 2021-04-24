ce2a6e2479bc5d1c6f136b9dd2e545c4e9b540ac
Jakub Norkiewicz <kubanorkiewicz@gmail.com>
2021-04-24 17:45:13 +0100

    add navigation, change menu

---
+++ src/components/nav/Navigation.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <button @click="previousStep" v-show="!isFirstStep()">
+            Back
+        </button>
+
+        <button @click="nextStep">
+            {{ nextStepButtonText() }}
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            isFirstStep() {
+                return this.$route.name === "input"
+            },
+            nextStepButtonText() {
+                if (this.$route.name === 'calculate') {
+                    return 'Submit'
+                } else {
+                    return 'Next'
+                }
+            },
+            previousStep() {
+                if (this.$route.name === 'calculate') {
+                    this.$router.push('/input')
+                } else if (this.$route.name === 'result') {
+                    this.$router.push('/calculate') 
+                }
+            },
+            nextStep() {
+                if (this.$route.name === 'calculate') {
+                    submit() 
+                } else if (this.$route.name === 'input') {
+                    this.$router.push('/calculate')
+                }
+            },
+            submit() {
+                alert('Submitted')
+            }
+        }   
+    }
+</script>
