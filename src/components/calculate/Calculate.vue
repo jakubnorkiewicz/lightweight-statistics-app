59e7c052d306a9b4744734e1e19bfd02725257c0
Jakub Norkiewicz <kubanorkiewicz@gmail.com>
2021-04-28 05:45:35 +0100

    add new formulas

--- src/components/calculate/Calculate.vue
+++ src/components/calculate/Calculate.vue
@@ -1,24 +1,40 @@
 <template>
 <h2>Your numbers:</h2>
-<h3>{{ dataset }}</h3>
-<input type="radio" id="mean" name="mean" value="mean" v-model="formula">
-<label for="mean">Mean</label><br>
-<input type="radio" id="weightedMean" name="weightedmean" value="weightedmean" v-model="formula">
-<label for="weightedmean">Weighted Mean</label><br>
-<input type="radio" id="median" name="median" value="median" v-model="formula">
-<label for="median">Median</label><br>
-<input type="radio" id="standardDeviation" name="standarddeviation" value="standarddeviation" v-model="formula">
-<label for="standarddeviation">Standard Deviation</label><br>
-<input type="radio" id="variance" name="variance" value="variance" v-model="formula">
-<label for="variance">Variance</label><br>
-<input type="radio" id="mode" name="mode" value="mode" v-model="formula">
-<label for="mode">Mode</label><br>
+<h3>{{ dataset }} </h3>
+<p>  {{ range() }}</p>
+<div class="form-check">
+<input class="form-check-input" type="radio" id="mean" name="mean" value="mean" v-model="formula">
+<label class="form-check-label" for="mean">Mean</label>
+</div>
+<div class="form-check">
+<input class="form-check-input" type="radio" id="weightedMean" name="weightedmean" value="weightedmean" v-model="formula">
+<label class="form-check-label" for="weightedmean">Weighted Mean</label>
+</div>
+<div class="form-check">
+<input class="form-check-input" type="radio" id="median" name="median" value="median" v-model="formula">
+<label class="form-check-label" for="median">Median</label>
+</div>
+<div class="form-check">
+<input class="form-check-input" type="radio" id="standardDeviation" name="standarddeviation" value="standarddeviation" v-model="formula">
+<label class="form-check-label" for="standarddeviation">Standard Deviation</label>
+</div>
+<div class="form-check">
+<input class="form-check-input" type="radio" id="variance" name="variance" value="variance" v-model="formula">
+<label class="form-check-label" for="variance">Variance</label>
+</div>
+<div class="form-check">
+<input class="form-check-input" type="radio" id="mode" name="mode" value="mode" v-model="formula">
+<label class="form-check-label" for="mode">Mode</label>
+</div>
+
 <div v-if="formula === 'mean'">{{ mean() }}</div>
 <div v-else-if="formula === 'weightedmean'">{{ weightedMean() }}</div>
-<div v-else-if="formula === 'median'"></div>
-<div v-else-if="formula === 'standarddeviation'"></div>
-<div v-else-if="formula === 'variance'"></div>
-<div v-else-if="formula === 'mode'"></div>
+<div v-else-if="formula === 'median'">{{ median() }}</div>
+<div v-else-if="formula === 'standarddeviation'">{{ standardDeviation() }}</div>
+<div v-else-if="formula === 'variance'">{{ variance() }}</div>
+<div v-else-if="formula === 'mode'">{{ mode() }}</div>
+<div v-else-if="formula === 'sum'">{{ sum() }}</div>
+
 
 
 </template>
@@ -40,7 +56,15 @@ export default {
   methods: {
     ...mapGetters([
       'mean',
-      'weightedMean'
+      'weightedMean',
+      'median',
+      'sum',
+      'variance',
+      'standardDeviation',
+      'mode',
+      'max',
+      'min',
+      'range'
     ])
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import { createStore } from 'vuex';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 import App from './App.vue';
 import Calculate from './components/calculate/Calculate.vue';
@@ -18,22 +20,55 @@ const store = createStore({
         },
     },
     getters: {
-        mean(state) {
-            let numArray = Array.from(state.dataset.split(','),Number)
-            if (numArray.length > 0) {
-                let sum = 0;
+        isDisable(state) {
+            return state.dataset.length > 0;
+        },
+
+        toArray(state) {
+            let numArray = Array.from(state.dataset.split(','), Number);
+            return numArray;
+        },
+
+        min(state, getters) {
+            let numArray = getters.toArray;
+            return Math.max.apply(null, numArray);
+
+        },
+
+        max(state, getters) {
+            let numArray = getters.toArray;
+            return Math.min.apply(null, numArray);
+        },
+
+        range(state, getters) {
+            let max = getters.max;
+            let min = getters.min;
+
+            return max + '-' + min;
+        }, 
+
+        sum(state, getters) {
+            let numArray = getters.toArray
+            let sum = 0;
                 numArray.forEach(n => { 
                  sum += n;
              });
-             return sum / numArray.length;
+             return sum;
+        },
+
+        mean(state, getters) {
+            let numArray = getters.toArray
+            if (numArray.length > 0) {
+             return getters.sum / numArray.length;
             }
         },
-        weightedMean(state) {
+
+        weightedMean(state, getters) {
             let weights = []
             let numbers = []
             let total_weight = 0;
             let weights_sum = 0;
-            let numArray = Array.from(state.dataset.split(','),Number)
+            let numArray = getters.toArray
 
 
             if(numArray.length % 2 !== 0) {
@@ -51,8 +86,89 @@ const store = createStore({
             }
             return (total_weight / weights_sum).toFixed(2);
 
+        },
+
+        median(state, getters) {
+            let numArray = getters.toArray
+
+            if(numArray.length === 0) {
+                return 0;
+            }
+            
+            //Sort array from smallest to largest number
+            numArray.sort(function(a,b){
+                return a-b;
+            });
+        
+            var half = Math.floor(numArray.length / 2);
+        
+            if (numArray.length % 2) {
+                return numArray[half];
+            }
+        
+                return (numArray[half - 1] + numArray[half]) / 2.0;
+        },
+
+        variance(state, getters) {
+            let numArray = getters.toArray;
+            //Calculate sum
+            let sum = getters.sum;
+            //Square the sum
+            let squaredSum = Math.pow(sum, 2);
+            //Divide the squared sum by the number of elements in the dataset
+            let dividedSquaredSum = squaredSum / numArray.length;
+            
+
+            let squaredNumbersSum = 0;
+            let variance = 0;
+            
+            //Square each element of the dataset individually and add them together
+            for(var i = 0; i < numArray.length; i++) {
+                squaredNumbersSum += Math.pow(numArray[i], 2)
+            }
+            //Substract sum of squared numbers from the squared sum divided by the number of elements in the dataset
+            let substractedSquaredNumbersSum = squaredNumbersSum - dividedSquaredSum;
+
+            variance = substractedSquaredNumbersSum / (numArray.length - 1);
+
+            console.log(squaredSum);
+            console.log(dividedSquaredSum);
+            console.log(numArray.length)
+            console.log(squaredNumbersSum)
+            return variance;
+        },
+
+        standardDeviation(state, getters) {
+            let variance = getters.variance;
+            let standardDeviation = Math.sqrt(variance);
+
+            return standardDeviation;
+        },
+
+        mode(state, getters) {
+            let numArray = getters.toArray
+            let frequency = {}; // object containing frequences of each element.
+            let bestOccurence = 0; // holds the max frequency.
+            let mode = [];
+
+            for (var i in numArray) {
+                frequency[numArray[i]] = (frequency[numArray[i]] || 0) + 1; // increment frequency.
+
+                if (frequency[numArray[i]] > bestOccurence) { // is this frequency > max so far ?
+                    bestOccurence = frequency[numArray[i]]; // update max.
+                }
+            }
+
+            for (var k in frequency) {
+                if (frequency[k] == bestOccurence) {
+                mode.push(k);
+                }
+            }
+
+            return mode;
         }
     }
+
 });
 
 const router = createRouter({
