8dac864e0cd3dcd89c50a7afc1a0352a7e7804ff
Jakub Norkiewicz <kubanorkiewicz@gmail.com>
2021-05-07 07:50:13 +0100

    fix getter call

--- src/pages/Calculate.vue
+++ src/pages/Calculate.vue
@@ -1,14 +1,12 @@
 <template>
   <div v-if="!isPopulated()">
-    <div class="alert alert-danger" role="alert">
-      Please go back to Input page and populate the field with numbers.
-    </div>
+   <alert-no-input></alert-no-input>
   </div>
   <div v-else>
     <div class="formulas card p-3">
       <div class="row">
         <div
-          class="col-sm-3"
+          class="col-sm-4"
           v-for="i in Math.ceil(calculations.length / 4)"
           :key="i.id"
         >
@@ -16,7 +14,7 @@
             v-for="calculation in calculations.slice((i - 1) * 4, i * 4)"
             :key="calculation.id"
           >
-            <div class="form-check">
+            <div class="form-check mt-3 mt-sm-0">
               <input
                 class="form-check-input"
                 type="checkbox"
@@ -67,8 +65,12 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import AlertNoInput from '../components/blocks/AlertNoInput.vue'
 
 export default {
+  components: {
+    AlertNoInput
+  },
   computed: {
     dataset: {
       get() {
@@ -98,4 +100,9 @@ export default {
 </script>
 
 <style scoped>
+@media only screen and (max-width: 575px) { 
+  .form-check label {
+    font-size: 20px;
+  }
+}
 </style>
